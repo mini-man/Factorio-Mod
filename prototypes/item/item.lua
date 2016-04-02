e40db1e050e8f03942c346e215785db3318898b1
Mini Man <ben.kirsh@hotmail.com>
2016-04-02 11:57:16 +1100

    Updated Some things

---
+++ prototypes/item/item.lua
@@ -0,0 +1,12 @@
+data:entend{
+{
+    {
+    type = "item",
+    name = "iron-ore",
+    icon = "__base__/graphics/icons/iron-ore.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "raw-resource",
+    order = "e[iron-ore]",
+    stack_size = 50
+  }
+  }
